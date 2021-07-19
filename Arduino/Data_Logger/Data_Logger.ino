7133374ec0c2c3ccfd9bcf72f3b498fc4012e63c
Victor Pavan <victor.pavan@univel.br>
2021-07-19 18:35:51 -0300

    Adicionado Novo LED de indicacao
    Adicionado modo de monitoramento USB
    
    Adicionado erro de verificacao de cartao
    
    Agora quando reiniciado, cria novo cabecalho no arquivo

--- Arduino/Data_Logger/Data_Logger.ino
+++ Arduino/Data_Logger/Data_Logger.ino
@@ -15,12 +15,15 @@
 //Configurações gerais (alterar de acordo com a necessidade)
 
 #define SERIALBAUD 9600
-#define action 8
+#define action_r 8
+#define action_g 9
 
 //Definicoes de variáveis globais
 unsigned int relogio[6]; //{hora, minuto, segundo, dia, mes, ano}
 #define CS 10
 bool pronto = false;
+bool cartao_ok = true;
+bool cartao_ok_led = false;
 
 //Definição dos barramentos I2C
 //SoftwareWire(sda, slc);
@@ -35,38 +38,57 @@ void setup(){
 	//Inicialização da comunicação serial
 	Serial.begin(SERIALBAUD);
 	//log(String("Inicializando comunicação serial a " + SERIALBAUD))
-	
+
+	//Inicializacao dos leds
+	pinMode(action_r, OUTPUT);
+	pinMode(action_g, OUTPUT);
+
 	//Inicializacao dos sensores nos barramentos I2C
 	for(int i=0; i < 3; i++){
 		hdc[i].begin(0x40, barramento[i]);
 		mlx[i].begin(barramento[i]);
 	}
 
+	//Teste do cartao
 	setupSdcard();
-	pinMode(action, OUTPUT);
+	//Tudo certo para comecar
+	digitalWrite(action_g, HIGH);
+
+	
+	//Cria cabecalho para marcar inicializacao
+	if(cartao_ok){
+		salvar("1", true);
+		delay(100);
+		salvar("2", true);
+		delay(100);
+		salvar("3", true);
+	}
+	
+
 	
 }
 
 void blink(byte code){
 	while(1){
-		digitalWrite(action, bitRead(code, 0));
+		digitalWrite(action_g, LOW);
+		digitalWrite(action_r, bitRead(code, 0));
 		delay(200);
-		digitalWrite(action, LOW);
-		delay(200);
-
-		digitalWrite(action, bitRead(code, 1));
-		delay(200);
-		digitalWrite(action, LOW);
+		digitalWrite(action_r, LOW);
 		delay(200);
 
-		digitalWrite(action, bitRead(code, 2));
+		digitalWrite(action_r, bitRead(code, 1));
 		delay(200);
-		digitalWrite(action, LOW);
+		digitalWrite(action_r, LOW);
 		delay(200);
 
-		digitalWrite(action, bitRead(code, 3));
+		digitalWrite(action_r, bitRead(code, 2));
 		delay(200);
-		digitalWrite(action, LOW);
+		digitalWrite(action_r, LOW);
+		delay(200);
+
+		digitalWrite(action_r, bitRead(code, 3));
+		delay(200);
+		digitalWrite(action_r, LOW);
 		delay(200);
 
 		delay(1000);
@@ -81,11 +103,11 @@ void timestep() {
 	if(RTC.read(tm)){
 
 		relogio[0] = tm.Hour;
-	   	relogio[1] = tm.Minute;
+        relogio[1] = tm.Minute;
 		relogio[2] = tm.Second;
 		relogio[3] = tm.Day;
 		relogio[4] = tm.Month;
-		relogio[5] = tm.Year;
+		relogio[5] = tmYearToCalendar(tm.Year);
 
 	}else{
 
@@ -100,38 +122,46 @@ void timestep() {
 }
 
 void loop(){
-/*
-	for(int i=0; i < 1; i++){
-		Serial.print("Casa ");
-		Serial.print(i+1);
-		Serial.println(": ");
-		Serial.println();
-		Serial.print("\t");
-		Serial.print("T=");
-		Serial.print(hdc[i].readTemperature());
-		Serial.print("C, UR=");
-		Serial.print(hdc[i].readHumidity());
-		Serial.print("%");
-		Serial.print("\t");
-		Serial.print("IR=");
-		Serial.println(mlx[i].readObjectTempC());
 
-
-	}
-	Serial.print("\033[0H\033[0J");
-*/
-	timestep();
-	if(relogio[1]%10 == 0){
-		if(!pronto){
-			salvar('1');
-			delay(100);
-			salvar('2');
-			delay(100);
-			salvar('3');
-			pronto = true;
+	for(int i=0; i < 3; i++){
+		if(hdc[i].readTemperature() < 124.00){
+			Serial.print(i+1);
+			Serial.print(';');
+			Serial.print(hdc[i].readTemperature());
+			Serial.print(';');
+			Serial.print(hdc[i].readHumidity());
+			Serial.print(';');
+			Serial.println(mlx[i].readObjectTempC());
+		}else{
+			Serial.print(i+1);
+			Serial.print(';');
+			Serial.print("N/A");
+			Serial.print(';');
+			Serial.print("N/A");
+			Serial.print(';');
+			Serial.println("N/A");
 		}
-	}else{
-		pronto = false;
 	}
-	delay(1000);
+	
+		
+
+	timestep();
+	if(!cartao_ok){
+	   	digitalWrite(action_g, cartao_ok_led);
+		cartao_ok_led = !cartao_ok_led;
+	}else{
+		if(relogio[2]%10 == 0){
+			if(!pronto){
+				salvar("1", false);
+				delay(100);
+				salvar("2", false);
+				delay(100);
+				salvar("3", false);
+				pronto = true;
+			}
+		}else{
+			pronto = false;
+		}
+	}
+	delay(500);
 }
---
+++ Arduino/Data_Logger/cartao.ino
@@ -0,0 +1,68 @@
+#include<SPI.h>
+#include<SdFat.h>
+
+#define CS 10
+
+SdFat sdCard;
+SdFile arquivo;
+
+void setupSdcard(){
+	if(!sdCard.begin(CS,SPI_HALF_SPEED)){
+		cartao_ok = false;
+	}	
+}
+
+void salvar(char *casa, bool cabecalho) {
+
+	int numerocasa = casa[0]-'0'-1;
+
+	digitalWrite(action_r, HIGH);
+
+	if (!arquivo.open(casa, O_RDWR | O_CREAT | O_AT_END)){
+		delay(300);
+		digitalWrite(action_r, LOW);
+		digitalWrite(action_g, LOW);
+		delay(500);
+		digitalWrite(action_r, HIGH);
+		digitalWrite(action_g, HIGH);
+
+	}else{
+
+		if(cabecalho){
+			arquivo.println("DATA; HORA; TEMP; UMIDADE; TEMP_IR");
+		}else{
+			arquivo.print(relogio[3]);
+			arquivo.print("/");
+			arquivo.print(relogio[4]);
+			arquivo.print("/");
+			arquivo.print(relogio[5]);
+			arquivo.print(";");
+			arquivo.print(relogio[0]);
+			arquivo.print(":");
+			arquivo.print(relogio[1]);
+			arquivo.print(";");
+
+			if(hdc[numerocasa].readTemperature() < 124.00){
+
+				arquivo.print(hdc[numerocasa].readTemperature());
+				arquivo.print(";");
+				arquivo.print(hdc[numerocasa].readHumidity());
+				arquivo.print(";");
+				arquivo.println(mlx[numerocasa].readObjectTempC());
+
+			}else{
+
+				arquivo.print("N/A");
+				arquivo.print(";");
+				arquivo.print("N/A");
+				arquivo.print(";");
+				arquivo.println("N/A");
+			}
+		}
+
+	}
+	arquivo.close();
+
+	delay(500);
+	digitalWrite(action_r, LOW);
+}
